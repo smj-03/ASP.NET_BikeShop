7db69c021ef34345c410400348d9841e938f5b70
smj-03 <chwastekszymon@gmail.com>
2025-06-03 01:33:40 +0200

    feat: configure database connection

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using BikeShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeShop.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) {
+    public DbSet<Bike> Bikes { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using BikeShop.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
