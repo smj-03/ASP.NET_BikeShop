4f82956d92ae22a69c1a0d3c61cf928e4ff29946
smj-03 <128691309+smj-03@users.noreply.github.com>
2025-06-03 15:08:49 +0200

    feat(CU-86c3pf05c): configure database (#1)
    * feat: configure database connection
    
    * feat: docker compose with sql server instance
    
    * fix: connection string
    
    * feat: initial database migration

---
+++ Migrations/20250602235828_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BikeShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bikes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ModelName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bikes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bikes");
+        }
+    }
+}
---
+++ Models/Bike.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BikeShop.Models;
+
+public class Bike {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string ModelName { get; set; }
+
+    [Required]
+    public double Price { get; set; }
+}
