88daac4a0c6cc246be08fcdc4f6a4b7ee6534f77
smj-03 <chwastekszymon@gmail.com>
2025-06-03 22:46:41 +0200

    refactor: format all code

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BikeShop.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) {
+public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
+{
     public DbSet<Bike> Bikes { get; set; }
 }
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,7 +1,8 @@
 namespace BikeShop.Models;
 
-public class ErrorViewModel {
+public class ErrorViewModel
+{
     public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
 }
