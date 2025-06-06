c8271f562c4b853c1fda96729b5b3e1db709f2be
Paweł Buczek <151944237+Buczy8@users.noreply.github.com>
2025-06-06 19:14:01 +0200

    feat: displaying products on the home page

--- Program.cs
+++ Program.cs
@@ -55,8 +55,9 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Account}/{action=Login}/{id?}")
+        pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
+
 
 app.Run();
 
