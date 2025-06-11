02320ba968e8c3928bd9a5633562c340002ed78a
Paweł Buczek <151944237+Buczy8@users.noreply.github.com>
2025-06-11 20:34:31 +0200

    feat: order list models and Dto

---
+++ Models/DTOs/OrderViewDto.cs
@@ -0,0 +1,12 @@
+﻿using BikeShop.Models.Enums;
+
+namespace BikeShop.Models;
+
+public class OrderViewDto
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public OrderStatus Status { get; set; }
+    public string CustomerEmail { get; set; }
+    public List<OrderProductDto> Items { get; set; }
+}
