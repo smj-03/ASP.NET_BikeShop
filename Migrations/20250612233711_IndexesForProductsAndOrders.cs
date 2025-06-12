abd7681ae7e26363ca35dbefde49a00a6b77852a
smj-03 <128691309+smj-03@users.noreply.github.com>
2025-06-13 01:41:04 +0200

    feat(CU-86c3x7aa0): database indexes (#16)
    * refactor: move dtos
    
    * feat: add database indexes

---
+++ Migrations/20250612233711_IndexesForProductsAndOrders.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BikeShop.Migrations
+{
+    public partial class IndexesForProductsAndOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Category",
+                table: "Products",
+                column: "Category");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Category",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders");
+        }
+    }
+}
