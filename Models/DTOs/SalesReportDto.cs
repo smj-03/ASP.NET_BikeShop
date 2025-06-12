11d6c7ad71c1334f776c4ead2896b0665fe427d0
Paweł Buczek <151944237+Buczy8@users.noreply.github.com>
2025-06-12 14:30:40 +0200

    feat: Dto for reports

---
+++ Models/DTOs/SalesReportDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BikeShop.Models;
+
+public class SalesReportDto
+{
+    public DateTime OrderDate { get; set; }
+    public string CustomerName { get; set; }
+    public List<OrderProductDto> Items { get; set; }
+    public decimal TotalPrice { get; set; }
+}
