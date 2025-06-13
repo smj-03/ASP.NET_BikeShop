a08b40e123788b8355ccf75957d52465cc94ade7
smj-03 <128691309+smj-03@users.noreply.github.com>
2025-06-13 11:50:49 +0200

    feat: add n log (#20)

--- Program.cs
+++ Program.cs
@@ -5,100 +5,118 @@ using BikeShop.Models;
 using BikeShop.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddScoped<IAccountService, AccountService>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<IUserSearchService, UserSearchService>();
-builder.Services.AddScoped<IRoleInitializer, RoleInitializer>();
-builder.Services.AddScoped<IOrderCommentService, OrderCommentService>();
-builder.Services.AddScoped<IReportService, ReportService>();
-
-builder.Services.AddScoped<UserMapper>();
-builder.Services.AddScoped<ProductMapper>();
-builder.Services.AddScoped<OrderMapper>();
-builder.Services.AddScoped<OrderCommentMapper>();
-
-builder.Services.AddScoped<BasketService>();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddSession();
-
-builder.Services.AddHostedService<OrderReportBackgroundService>();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
+var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+try
 {
-    var initializer = scope.ServiceProvider.GetRequiredService<IRoleInitializer>();
-    await initializer.EnsureRolesExistAsync();
+    var builder = WebApplication.CreateBuilder(args);
 
-    // Create default admin user if not exists
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var adminEmail = "admin@mail.com";
-    var adminPassword = "Admin!123";
-    var adminUser = await userManager.FindByEmailAsync(adminEmail);
-    if (adminUser == null)
+    // Remove default logging providers and use NLog
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+    builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
+
+    builder.Services.AddScoped<IAccountService, AccountService>();
+    builder.Services.AddScoped<IProductService, ProductService>();
+    builder.Services.AddScoped<IOrderService, OrderService>();
+    builder.Services.AddScoped<IUserSearchService, UserSearchService>();
+    builder.Services.AddScoped<IRoleInitializer, RoleInitializer>();
+    builder.Services.AddScoped<IOrderCommentService, OrderCommentService>();
+    builder.Services.AddScoped<IReportService, ReportService>();
+
+    builder.Services.AddScoped<UserMapper>();
+    builder.Services.AddScoped<ProductMapper>();
+    builder.Services.AddScoped<OrderMapper>();
+    builder.Services.AddScoped<OrderCommentMapper>();
+
+    builder.Services.AddScoped<BasketService>();
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddSession();
+
+    builder.Services.AddHostedService<OrderReportBackgroundService>();
+
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
     {
-        adminUser = new ApplicationUser
-        {
-            UserName = adminEmail,
-            Email = adminEmail,
-            EmailConfirmed = true,
-            FirstName = "Admin",
-            LastName = "Admin",
-            PhoneNumber = "1234567890",
-        };
-        var result = await userManager.CreateAsync(adminUser, adminPassword);
-        if (result.Succeeded)
-        {
-            if (!await roleManager.RoleExistsAsync("Admin"))
-            {
-                await roleManager.CreateAsync(new IdentityRole("Admin"));
-            }
+        var initializer = scope.ServiceProvider.GetRequiredService<IRoleInitializer>();
+        await initializer.EnsureRolesExistAsync();
 
-            await userManager.AddToRoleAsync(adminUser, "Admin");
+        // Create default admin user if not exists
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var adminEmail = "admin@mail.com";
+        var adminPassword = "Admin!123";
+        var adminUser = await userManager.FindByEmailAsync(adminEmail);
+        if (adminUser == null)
+        {
+            adminUser = new ApplicationUser
+            {
+                UserName = adminEmail,
+                Email = adminEmail,
+                EmailConfirmed = true,
+                FirstName = "Admin",
+                LastName = "Admin",
+                PhoneNumber = "1234567890",
+            };
+            var result = await userManager.CreateAsync(adminUser, adminPassword);
+            if (result.Succeeded)
+            {
+                if (!await roleManager.RoleExistsAsync("Admin"))
+                {
+                    await roleManager.CreateAsync(new IdentityRole("Admin"));
+                }
+
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
         }
     }
-}
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseStaticFiles();
+    app.UseSession();
+    app.MapStaticAssets();
+
+    app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Home/Error");
-
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseStaticFiles();
-app.UseSession();
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-app.Run();
+finally
+{
+    NLog.LogManager.Shutdown();
+}
